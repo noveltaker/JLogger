f08f111cadbcdd0c16f82f12670c06bc2fc0b8cc
gon-zo <pnw123456@icloud.com>
2022-04-15 18:21:35 +0900

    git caced apply

--- src/main/kotlin/org/noveltaaker/jlogger/AbstractJLogger.kt
+++
@@ -1,7 +0,0 @@
-package org.noveltaaker.jlogger
-
-import org.slf4j.LoggerFactory
-
-abstract class AbstractJLogger : JLoggerValue {
-    val logger = LoggerFactory.getLogger(FULL_NAME)
-}
--- src/main/kotlin/org/noveltaaker/jlogger/JLogger.kt
+++
@@ -1,9 +0,0 @@
-package org.noveltaaker.jlogger
-
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-interface JLogger : JLoggerValue {
-    val logger: Logger
-        get() = LoggerFactory.getLogger(FULL_NAME)
-}
