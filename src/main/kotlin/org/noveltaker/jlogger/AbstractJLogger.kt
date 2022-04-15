b5a0fee9dc1d622b2423b31077aa4b72c4e6a51a
gon-zo <pnw123456@icloud.com>
2022-04-15 18:51:03 +0900

    remove JLoggerValue kt file

--- src/main/kotlin/org/noveltaker/jlogger/AbstractJLogger.kt
+++ src/main/kotlin/org/noveltaker/jlogger/AbstractJLogger.kt
@@ -2,6 +2,14 @@ package org.noveltaker.jlogger
 
 import org.slf4j.LoggerFactory
 
-abstract class AbstractJLogger : JLoggerValue {
+abstract class AbstractJLogger {
     val logger = LoggerFactory.getLogger(FULL_NAME)
+
+    private val Any.FULL_NAME: String
+        get() {
+            val tag = this.javaClass.simpleName
+            val className = if (tag.length <= 23) tag else tag.substring(0, 23)
+            val packageName = this.javaClass.packageName
+            return "$packageName.$className"
+        }
 }
--- src/main/kotlin/org/noveltaker/jlogger/JLogger.kt
+++ src/main/kotlin/org/noveltaker/jlogger/JLogger.kt
@@ -3,7 +3,15 @@ package org.noveltaker.jlogger
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-interface JLogger : JLoggerValue {
+interface JLogger {
     val logger: Logger
         get() = LoggerFactory.getLogger(FULL_NAME)
+
+    private val Any.FULL_NAME: String
+        get() {
+            val tag = this.javaClass.simpleName
+            val className = if (tag.length <= 23) tag else tag.substring(0, 23)
+            val packageName = this.javaClass.packageName
+            return "$packageName.$className"
+        }
 }
